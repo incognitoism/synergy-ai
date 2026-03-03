58ef9d213ed6d81e01e9a79023f8efe80cd4a6bb
XQC ULTIMA <xqcultima@XQCs-MacBook-Air.local>
2026-03-03 15:28:38 +0530

    Composite login2

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,14 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest } from "next/server";
 
 export default withAuth(
-    async function middleware(req) {
+    async function middleware(req: NextRequest) {
     },
     {
-        // Middleware still runs on all routes, but doesn't protect the home route
-        publicPaths: ["/"], // e.g. ["/api/public", "/blog", "/about"]
+        publicRoutes: ["/"],
     }
 );
 
 export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    ],
-}
+    matcher: ["/((?!_next|static|favicon.ico).*)"],
+};
